b777332fa705a8babf0a6c74d531dd7409fccf21
nickthehacker <nickblackburn02@gmail.com>
2018-07-08 10:51:10 -0400

    S

---
+++ build/entry.d
@@ -0,0 +1,8 @@
+entry.o: \
+ /home/nickthehacker/Desktop/homebrew/ProgrammingOnTheU/resources/3-1-Hullo-World/src/entry.c \
+ /home/nickthehacker/Desktop/homebrew/ProgrammingOnTheU/resources/3-1-Hullo-World/src/hbl.h \
+ /home/nickthehacker/Desktop/homebrew/ProgrammingOnTheU/resources/3-1-Hullo-World/src/wiiu.h
+
+/home/nickthehacker/Desktop/homebrew/ProgrammingOnTheU/resources/3-1-Hullo-World/src/hbl.h:
+
+/home/nickthehacker/Desktop/homebrew/ProgrammingOnTheU/resources/3-1-Hullo-World/src/wiiu.h:
